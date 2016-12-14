a992170ebc9dbe99538240ea8ea27e4b705612e9
zadannu <danu93azuga@gmail.com>
2016-12-14 11:00:10 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * AT128.c
+ *
+ * Created: 12/8/2016 8:05:23 AM
+ * Author : bogdan.slujitoru
+ */ 
+
+#include <avr/io.h>
+
+void USART_Init( unsigned int ubrr );
+void USART_Transmit( unsigned char data );
+unsigned char USART_Receive( void );
+
+int main(void)
+{
+   		/* to do: 
+			initializare taskuri
+		    generare pwm pe baza datelor primite
+			sincronizare taskuri 
+			 */ 
+		   
+		   
+}
+
